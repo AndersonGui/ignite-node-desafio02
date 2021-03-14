d705016527d7740c43d56732051717e26baa2370
Anderson <andersogui@gmail.com>
2021-03-14 01:18:01 -0300

    Desafio Realizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(u => u.username === username);
+
+  if (!user) {
+    return response.status(404);
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length > 9) {
+    return response.status(403);
+  };
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Erro" });
+  }
+
+  const user = users.find(u => u.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "Erro" });
+  }
+
+  const todo = user.todos.find(t => t.id == id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Erro" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(u => u.id == id);
+
+  if (!user) {
+    return response.status(404);
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
